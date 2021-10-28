4314a904879b76ee33d528e08bf346c0dd7819f4
josephlitribe <josephli036@gmail.com>
2021-10-27 19:45:46 -0700

    Add initial options + colorsr

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,5 +5,32 @@ import "vuetify/styles";
 // Vuetify
 import { createVuetify } from "vuetify";
 
-export default createVuetify();
+const options = {
+  theme: {
+    themes: {
+      light: {
+        primary: "#212121",
+        accent: "#8362E5",
+        secondary: "#607D8B",
+        info: "#03A9F4",
+        warning: "#FFB300",
+        error: "#F44336",
+        success: "#00C853",
+        background: "#ECEFF1",
+      },
+      dark: {
+        primary: "#212121",
+        accent: "#8362E5",
+        secondary: "#607D8B",
+        info: "#03A9F4",
+        warning: "#FFB300",
+        error: "#F44336",
+        success: "#00C853",
+        background: "#ECEFF1",
+      },
+    },
+  },
+};
+
+export default createVuetify(options);
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
